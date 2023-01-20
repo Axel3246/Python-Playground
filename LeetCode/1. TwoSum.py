080f5156ea580750702eef0d9de0fa7cbab8c38d
Axel HC <hplaxel@gmail.com>
2023-01-19 18:11:09 -0600

    1. TwoSum
    Added example of hashmap usage

--- LeetCode/1. TwoSum.py
+++ LeetCode/1. TwoSum.py
@@ -17,3 +17,25 @@ class Solution:
         
 # @lc code=end
 
+### HASHMAP SOLUTION (TOP)
+
+nums = [3,2,4]
+target = 6
+
+# EMPTY HASHMAP
+hashmap = {}
+# TRAVERSING NUM ARRAY
+for i in range(len(nums)):
+    # GETTING THE RESIDUAL VALUE OF NUMS - TARGET. THIS WAY WE'LL BE ABLE TO FIND LATER IF THERE EXIST 
+    # A NUM[I] THAT IS A COMPLIMENT OF ANOTHER NUMBER
+    complement = target - nums[i]
+    #print(f'Complement es {complement}')
+    # WHEN WE FIND IT WE RETURN THE INDEXES
+    if complement in hashmap:
+        print ([i, hashmap[complement]])
+    # ELSE WE INCLUDE NUMS[I] AND THE ITERATOR IN THE HASHMAP
+    hashmap[nums[i]] = i
+    #print(f'En {i} el hash tiene',hashmap)
+    
+#print(hashmap)
+    
