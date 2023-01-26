ece35a116e5767d8f865a3347e74f6de8e74d02b
Axel HC <hplaxel@gmail.com>
2023-01-25 22:03:30 -0600

    1929. Concatenation of Array
    LC#1929 Implementation

---
+++ LeetCode/Easy/1929. Concatenation of Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        return ans + nums + nums
+    
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums*2
