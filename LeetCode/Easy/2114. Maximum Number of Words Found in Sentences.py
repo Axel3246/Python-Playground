c1b2da8d9dcf9aea2b360e2afed7a058c4995f16
Axel HC <hplaxel@gmail.com>
2023-01-26 20:24:12 -0600

    2114. Maximum Number of Words Found in Sentences

---
+++ LeetCode/Easy/2114. Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,28 @@
+'''
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+
+ 
+
+Example 1:
+
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation: 
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+'''
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        hi = 0
+        for i in sentences:
+            size = len(i.split(" ")) # Count words in sentence
+            if size > hi: # update max
+                hi = size
+        return hi
