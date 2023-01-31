57afd89e083ba2468962288553ab92e7ab76782f
Axel HC <hplaxel@gmail.com>
2023-01-31 02:18:10 -0600

    49. Group Anagrams

--- LeetCode/Medium/49. Group Anagrams.py
+++ LeetCode/Medium/49. Group Anagrams.py
@@ -1,3 +1,20 @@
+'''
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+'''
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # MAPPING EACH STRING, USING DEFAULT DICT TO AVOID EMPTY KEYS
