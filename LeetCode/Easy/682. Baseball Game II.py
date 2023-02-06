d2baa29d664747d8b17d00996e4578b2a71bbbc2
Axel HC <hplaxel@gmail.com>
2023-02-06 16:53:05 -0600

    682. Baseball Game
    A more simple approach. (SIMPLIFIED)

---
+++ LeetCode/Easy/682. Baseball Game II.py
@@ -0,0 +1,23 @@
+'''
+Simplier Implementation
+'''
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+
+        stack=[]
+
+        for op in operations:
+
+            if op =='+':
+                stack.append(stack[-1] + stack[-2])
+
+            elif op =='D':
+                stack.append(2* stack[-1])
+
+            elif op =='C':
+                stack.pop()
+          
+            else:
+                stack.append(int(op))
+
+        return sum(stack)
