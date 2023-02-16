5050d98d7cd6f1fe755432cb4829a21c6f95d184
Axel HC <hplaxel@gmail.com>
2023-02-16 10:18:26 -0600

    Create 1816. Truncate Sentence.py

---
+++ LeetCode/Easy/1816. Truncate Sentence.py
@@ -0,0 +1,23 @@
+'''
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. 
+Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. 
+Return s​​​​​​ after truncating it.
+
+Example 1:
+
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+'''
+
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        # Split sentence and return the correct .join length
+        arr = s.split()
+        return " ".join(arr[0:k])
